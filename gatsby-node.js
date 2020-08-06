87c4dd5bd2d66ca215a355d7ab59cfce1a36a3eb
newwaveds <nwdigitalsolutions19@gmil.com>
2020-08-06 11:25:33 -0700

    Update gatsby-node.js
    Correction to path.resolve

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,8 +8,7 @@ const path = require(`path`)
 
 exports.createPages = ({ graphql, actions}) => {
 	const { createPage } = actions
-	const blogPostTemplate = path.resolve
-	(`src/templates/blog-post.js`)
+	const blogPostTemplate = path.resolve(`src/templates/blog-post.js`)
 	return graphql(`
 		query PostsQuery {
 			wordPress {
